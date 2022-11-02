d1913ff4634c1968ad1291d90d1e7f8f6f40cb07
Chipes12 <carlos.andres_paez@outlook.com>
2022-11-02 16:48:34 -0600

    rent button

--- lib/pages/check_material.dart
+++ lib/pages/check_material.dart
@@ -1,5 +1,30 @@
 import 'package:flutter/material.dart';
 
+enum WeekDay {
+  Lunes,
+  Martes,
+  Miercoles,
+  Jueves,
+  Viernes, 
+  Sabado,
+  Domingo
+}
+
+enum Month {
+  Enero,
+  Febrero,
+  Marzo,
+  Abril,
+  Mayo, 
+  Junio,
+  Julio,
+  Agosto,
+  Septiembre,
+  Octubre,
+  Noviembre,
+  Diciembre
+}
+
 class MaterialDetail extends StatefulWidget {
   final dynamic material;
   const MaterialDetail({Key? key, required this.material}) : super(key: key);
@@ -62,6 +87,18 @@ class _MaterialDetailState extends State<MaterialDetail> {
                             textAlign: TextAlign.justify,
                           ),
                         ),
+                      ),
+                      SizedBox(height: 20),
+                      MaterialButton(
+                        hoverColor: Colors.blueAccent,
+                        disabledColor: Colors.grey,
+                        height: 20,
+                        minWidth: 50,
+                        color: Colors.blue,
+                        onPressed: material["disponibles"] == 0 ? null : () {
+                          _pickUpMaterial();
+                        },
+                        child: Text("Rentar", style: TextStyle(color: Colors.white),),
                       )
                     ],
                   ),
@@ -70,6 +107,52 @@ class _MaterialDetailState extends State<MaterialDetail> {
             ),
           ),
         ));
+  }
+
+
+  bool _isTodayLabourDay(int day){
+    return (day >= 1 && day <= 5);
+  }
+  
+  bool _isTomorrowLabourDay(int day){
+    day++;
+    return (day  >= 1 && day <= 5);
+  }
+
+  String _whenToPick(){
+    var when;
+    var dt = DateTime.now();
+    if (dt.hour < 19 && _isTodayLabourDay(dt.weekday)){
+      when = "hoy mismo ";
+    } else if(dt.hour > 19 && _isTomorrowLabourDay(dt.weekday)){
+        dt = dt.add(Duration(days: 1));
+        when =  "mañana ";
+    } else {
+      dt = dt.add(Duration(days: (DateTime.monday - dt.weekday) % DateTime.daysPerWeek));
+      when = "el proximo ";
+    }
+    when += "${WeekDay.values[dt.weekday - 1].toString().split('.').last} ${dt.day} de ${Month.values[dt.month - 1].toString().split('.').last} de ${dt.year}";
+    return when;
+  }
+
+    void _pickUpMaterial() {
+    showDialog(
+      context: context,
+      builder: ((context) {
+        return AlertDialog(
+          title: Text("Rentado exitosamente por 7 días"),
+          content: Text("Pasa a biblioteca a recoger tu material a partir de ${_whenToPick()}\nTienes todo tu periodo para recogerlo\nDisponible de 8am a 7pm"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Entendido"),
+            )
+          ],
+        );
+      }),
+    );
   }
 }
 
