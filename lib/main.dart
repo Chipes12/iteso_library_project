cf77759ac393c270f65c3856892f8667cf51791b
NelsonIVL <is708203@iteso.mx>
2022-11-27 16:15:00 -0600

    Firebase ffuncionando

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -6,15 +7,18 @@ import 'package:iteso_library_project/pages/blocCamera/bloc/camera_bloc_bloc.dar
 import 'package:iteso_library_project/pages/logIn/logIn.dart';
 import 'package:iteso_library_project/pages/logIn/login2.dart';
 
-void main() =>
-    initializeDateFormatting().then((_) => runApp(MultiBlocProvider(providers: [
-          BlocProvider(
-            create: (context) => CameraBlocBloc(),
-          ),
-          BlocProvider(
-            create: (context) => DataFireBBloc(),
-          ),
-        ], child: MyApp())));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  initializeDateFormatting().then((_) => runApp(MultiBlocProvider(providers: [
+        BlocProvider(
+          create: (context) => CameraBlocBloc(),
+        ),
+        BlocProvider(
+          create: (context) => DataFireBBloc(),
+        ),
+      ], child: MyApp())));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -25,7 +29,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'ITESO Library',
       theme: ThemeData(primarySwatch: Colors.indigo),
-      home: logIn(),
+      home: Login2(),
     );
   }
 }
