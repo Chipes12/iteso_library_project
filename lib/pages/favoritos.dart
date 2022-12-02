7b477a781502dcd784579a12d4c852006f403102
NelsonIVL <is708203@iteso.mx>
2022-12-01 22:41:44 -0600

    Implementacion QR

--- lib/pages/favoritos.dart
+++ lib/pages/favoritos.dart
@@ -1,7 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Favoritos extends StatelessWidget {
-  const Favoritos({super.key});
+  final dynamic favorites = FirebaseFirestore.instance
+      .collection('users')
+      .doc(FirebaseAuth.instance.currentUser!.uid)
+      .snapshots();
+
+  Favoritos({super.key});
 
   @override
   Widget build(BuildContext context) {
